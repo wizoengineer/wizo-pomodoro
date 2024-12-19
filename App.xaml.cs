5ea59f3b4eba53beb9c542e420f4cd2960bbde99
wizoengineer <neduska99@gmail.com>
2024-12-19 13:01:08 +0000

    Fixed window

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,17 @@ public partial class App : Application
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new AppShell());
+		var window = new Window(new AppShell());
+
+#if MACCATALYST || WINDOWS
+		window.Width = 400;
+		window.Height = 600;
+		window.MinimumWidth = 400;
+		window.MinimumHeight = 600;
+		window.MaximumWidth = 400;
+		window.MaximumHeight = 600;
+#endif
+
+		return window;
 	}
 }
